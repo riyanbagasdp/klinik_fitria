6211da4f6490238e613da0e9fcec7c0e362ee9c4
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2022-04-26 12:27:35 +0700

    riyan

--- application/views/dokter/dokter.php
+++ application/views/dokter/dokter.php
@@ -359,7 +359,7 @@
 						<div class="col-lg-12 grid-margin stretch-card">
 							<div class="card">
 								<div class="card-body">
-									<h4 class="card-title">Data Pasien</h4>
+									<h4 class="card-title">Data Dokter</h4>
 									<div class="table-responsive">
 										<table class="table table-hover" id="dokter">
 											<thead class="table-info">
