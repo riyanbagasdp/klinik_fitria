d30fcc7cfc686c1fe2e305a99bbb32681a2c9c94
hanizarfs <hanizar.dev@gmail.com>
2022-04-27 02:17:22 +0700

    hanizar

--- application/controllers/Pasien.php
+++ application/controllers/Pasien.php
@@ -44,8 +44,10 @@ class Pasien extends CI_Controller {
 			'no_telp' => $no_telp,
 		);
 
-		$this->pasienModel->insert_data($data, 'pasiens');
-		redirect(base_url('pasien'));
+		if($this->pasienModel->insert_data($data, 'pasiens')) {
+			$this->session->set_flashdata('pesan', 'Data berhasil di tambahkan');
+			redirect(base_url('pasien'));
+		}
 	}
 
 	// Function untuk mengarahkan ke tampilan edit
@@ -79,14 +81,11 @@ class Pasien extends CI_Controller {
 			'no_telp' => $no_telp,
 		);
 
-		$this->pasienModel->update_data($data, $id_pasien);
-		redirect(base_url('pasien'));
+		if($this->pasienModel->update_data($data, $id_pasien)) {
+			$this->session->set_flashdata('pesan', 'Data berhasil di ubah');
+			redirect(base_url('pasien'));
+		}
     }
-
-	// public function delete($id_pasien) {
-	// 	$this->pasienModel->delete_data($id_pasien);
-	// 	redirect(base_url('pasien'));
-	// }
 
 	public function delete($id_pasien) {
 		if($this->pasienModel->delete_data($id_pasien)) {
